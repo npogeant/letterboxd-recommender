af6f53ae3ad4083bd0afe4222bef3b952f0a017d
Nicolas <64933282+npogeant@users.noreply.github.com>
2024-03-29 17:55:49 +0000

    added a metadata for the app to accept http request

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <body>{children}</body>
     </html>
   );
